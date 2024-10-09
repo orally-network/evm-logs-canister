8a012e206d90e7d76a8cadfc67247a1945b6cfce
Oleksandr Yemets <alexxandryemets@gmail.com>
2024-10-09 14:19:49 +0300

    test canister updated to pass filters via command args

--- src/test_canister/src/lib.rs
+++ src/test_canister/src/lib.rs
@@ -1,6 +1,7 @@
 use ic_cdk::api::call::call;
-use candid::Principal;
+use candid::{Principal, CandidType};
 use ic_cdk_macros::{update, query, init};
+use serde::Deserialize;
 use std::cell::RefCell;
 
 use evm_logs_types::{SubscriptionRegistration, RegisterSubscriptionResult, EventNotification, ICRC16Map, ICRC16Value};
@@ -15,30 +16,7 @@ async fn init() {
 }
 
 #[update]
-async fn call_icrc72_register_subscription() {
-    // TODO set canister_id during init method
-    let canister_id = Principal::from_text("bkyz2-fmaaa-aaaaa-qaaaq-cai").unwrap();
-
-    let namespaces = vec![
-        "com.example.myapp.events.Ethereum",
-        "com.example.myapp.events.Optimism",
-        "com.example.myapp.events.Base",
-    ];
-
-    let registrations: Vec<SubscriptionRegistration> = namespaces
-        .into_iter()
-        .map(|namespace| SubscriptionRegistration {
-            namespace: namespace.to_string(),
-            config: vec![
-                ICRC16Map {
-                    key: ICRC16Value::Text("icrc72:subscription:filter".to_string()),
-                    value: ICRC16Value::Text("address == 0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852".to_string()),
-                },
-            ],
-            memo: None,
-        })
-        .collect();
-
+async fn call_icrc72_register_subscription(canister_id: Principal, registrations: Vec<SubscriptionRegistration>) {
     ic_cdk::println!("Calling icrc72_register_subscription for namespaces:");
     for reg in &registrations {
         ic_cdk::println!(" - {:?}", reg.namespace);
@@ -66,7 +44,7 @@ async fn icrc72_handle_notification(notification: EventNotification) {
     ic_cdk::println!("Received notification for event ID: {:?}", notification.event_id);
     ic_cdk::println!("Notification details: {:?}", notification);
 
-    // Store the received notification
+    // Зберігання отриманого сповіщення
     NOTIFICATIONS.with(|notifs| {
         notifs.borrow_mut().push(notification);
     });
@@ -76,5 +54,3 @@ async fn icrc72_handle_notification(notification: EventNotification) {
 fn get_notifications() -> Vec<EventNotification> {
     NOTIFICATIONS.with(|notifs| notifs.borrow().clone())
 }
-
-
